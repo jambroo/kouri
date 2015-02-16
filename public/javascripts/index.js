23bb3735d3c3568b34642374c1c41a5857e6b9ff
James Brooking <jambroo@gmail.com>
2015-02-16 15:51:20 +0100

    Removed tooltips.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,9 +1,27 @@
 $(function() {
   $( "#date" ).datepicker({
-	  dateFormat: "yy-mm-dd"
-	});
-});
+	  dateFormat: "yy-mm-dd",
+    beforeShow: function() {
+        setTimeout(function(){
+            $('.ui-datepicker').css('z-index', 99999999999999);
+        }, 0);
+    }
+	})
 
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+  $('#run').keyup(function(e) {
+  	var runEl = $(e.target);
+  	var run = runEl.val();
+    
+    $.get("/common", function(data) {
+    	for (lap in data) {
+    		try {
+	    		run = run.replace(new RegExp(lap, "g"), data[lap])
+	    		var runEval = eval(run);
+	    		runEl.siblings('span').html(runEval+' km');
+	    	} catch(err) {
+	    		runEl.siblings('span').html('? km');
+	    	}
+    	}
+    })
+  })
 })

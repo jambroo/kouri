d8309cbb3b1311683b301f51d3d9609b804d32f1
James Brooking <jambroo@gmail.com>
2015-02-17 22:04:47 +0100

    Added error handling.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var moment = require('moment');
+var expressValidator = require('express-validator');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -22,12 +23,18 @@ app.locals.today = function(val) {
     return moment().format('YYYY-MM-DD')
 }
 
+var isDate = function(d) {
+    var re = /^\d{4}-\d{1,2}-\d{1,2}$/
+    return re.test(d)
+}
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressValidator({customValidators: {isDate: isDate}}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var util = require('util');
 
 var runs = [{date: '2015-01-14', distance: 10, time: 490.3}]
 var commonDistances = {toPark: {label: 'To Park', distance:	1.1},
@@ -8,9 +9,37 @@ var commonDistances = {toPark: {label: 'To Park', distance:	1.1},
 	toLakes: {label: 'To Lakes', distance: 2.4},
 	lakesLap: {label: 'Lakes Lap', distance: 6.7}}
 
+// Get full common lap info
+router.get('/commonFull', function(req, res, next) {
+  res.json(commonDistances);
+});
+
+// Get only common lap distances
+router.get('/common', function(req, res, next) {
+  var commonSlim = {}
+  for (i in commonDistances) {
+  	commonSlim[i] = commonDistances[i].distance
+	}
+  res.json(commonSlim)
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Kouri!', runs: runs, commonDistances: commonDistances });
+});
+
+/* POST home page. */
+router.post('/', function(req, res) {
+  req.assert('date', 'required').notEmpty().len(10).isDate();
+  req.assert('run', 'required').notEmpty();
+  req.assert('time', 'required').notEmpty();
+
+  console.log(req.validationErrors(true))
+
+  res.render('index', { title: 'Kouri!',
+    runs: runs,
+    commonDistances: commonDistances,
+    errors: req.validationErrors(true) });
 });
 
 module.exports = router;
